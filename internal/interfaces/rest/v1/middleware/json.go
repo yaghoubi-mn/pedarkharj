c01b0cc84cb37b1959117901e2180b734c0e83c6
Mohammadmin Yaghoubi <yaghoubi.mn@gmail.com>
2024-10-01 11:13:11 +0330

    fix cors

--- internal/interfaces/rest/v1/middleware/json.go
+++ internal/interfaces/rest/v1/middleware/json.go
@@ -21,17 +21,17 @@ func NewJsonMiddleware(response datatypes.Response) jsonMiddleware {
 func (j *jsonMiddleware) EnsureApplicationJson(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-		if r.Method == "OPTIIONS" {
+		if r.Method == "POST" {
 
-			w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
-			w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, DELETE, PUT")
-			w.Header().Set("Access-Control-Allow-Headers", "content-type, access-control-allow-origin, accept, user-agent, authorization")
-			w.Header().Set("Access-Control-Allow-Max-Age", "86400")
-			return
-		}
-		if r.Header.Get("Content-Type") != "application/json" {
-			j.response.ErrorResponse(w, 400, rcodes.InvalidHeader, errors.New("header application/json is required"))
-			return
+			// w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+			// w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, DELETE, PUT")
+			// w.Header().Set("Access-Control-Allow-Headers", "content-type, access-control-allow-origin, accept, user-agent, authorization")
+			// w.Header().Set("Access-Control-Allow-Max-Age", "86400")
+			// return
+			if r.Header.Get("Content-Type") != "application/json" {
+				j.response.ErrorResponse(w, 400, rcodes.InvalidHeader, errors.New("header application/json is required"))
+				return
+			}
 		}
 		// w.Header().Add("")
 		next.ServeHTTP(w, r)
